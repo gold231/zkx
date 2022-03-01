1df0ad5d0711cb8a0d042c5dfd509efafd9230cb
gold231 <goldendragon0318@gmail.com>
2022-03-01 03:19:48 -0500

    header and about us update

--- src/components/AboutUsSection/AboutUsSection.js
+++ src/components/AboutUsSection/AboutUsSection.js
@@ -1,17 +1,19 @@
+
+
 const AboutUsSection = () => {
     return (
 		<div class="container mx-auto lg:px-20 xl:px-20 px-4 min-h-90" id="aboutussection">
 			<div class=" flex-wrap  justify-center lg:mt-20 lg:mt-20 mt-10 text-left">
-				<div class="w-full sm:full md:w-full md:px-0 md:py-0 md:flex md:flex-col md:justify-center md:items-center lg:py-0 xl:py-0 lg:px-0 xl:px-0 lg:w-full  xl:w-2/4  gap-x-7 lg:m-0 xl:m-0 mr-0 xl:ml-0 xl:float-right">
-					<div class="bg-[#1e2029] xl:w-11/12 lg:w-full  rounded-3xl p-8 lg:flex  xl:flex-col lg:flex-col xl:flex lg:px-[42px] py:40 xl:px-[42px] lg:py-[78px]  md:px-[42px] md:py-[78px] xl:py-[78px]">
+				<div class="w-full xl:pl-4 xl:items-end sm:full md:w-full md:px-0 md:py-0 md:flex md:flex-col md:justify-center md:items-center lg:py-0 xl:py-0 lg:px-0 xl:px-0 lg:w-full  xl:w-2/4  gap-x-7 lg:m-0 xl:m-0 mr-0 xl:ml-0 xl:float-right">
+					<div class="bg-[#1e2029] xl:w-full lg:w-full  rounded-3xl p-8 lg:flex  xl:flex-col lg:flex-col xl:flex lg:px-[42px] py:40 xl:px-[42px] lg:py-[78px]  md:px-[42px] md:py-[78px] xl:py-[94px]">
 						<h1 class="text-4xl text-left text-[#00ff62] font-bold ">About Us</h1>
 						<p class="text-white text-lg mt-5 font-sir">ZKX is an innovative derivatives protocol built on StarkNet, StarkWare’s ZK rollup.  We are introducing power perpetuals, an evolution of perpetual swaps with high convexity and hedged downside.</p>
 						<p class="text-white text-lg mt-5 font-sir" >Elevate your trading experience with gamified leaderboards and choose from multiple categories of off-chain and on-chain assets.</p>
 						<p class="text-white text-lg mt-5 font-sir">Our mission is to democratize access to global yields through our offerings to anyone, anywhere.</p>
 					</div>
 				</div>
-				<div class="sm:w-full w-full md:px-0 xl:px-0 lg:px-0 md:flex md:flex-col md:justify-center md:items-center md:py-2 item-center md:w-full lg:w-full  xl:w-2/4 mb-4 gap-x-7  xl:py-0 lg:py-0 xl:p-0 lg:p-0 ml-0 mt-4">
-					<div class="bg-[#1e2029] xl:w-11/12 lg:w-full md:w-full md:justify-center w-full rounded-3xl p-12 lg:flex md:flex  p-[42px] xl:p-20 lg:p-20 xl:flex-col lg:flex-col xl:flex">
+				<div class="sm:w-full xl:pr-4 xl:items-start w-full md:px-0 xl:px-0 lg:px-0 md:flex md:flex-col md:justify-center md:items-center md:py-2 item-center md:w-full lg:w-full  xl:w-2/4 mb-4 gap-x-7  xl:py-0 lg:py-0 xl:p-0 lg:p-0 ml-0 mt-4">
+					<div class="bg-[#1e2029] xl:w-full lg:w-full md:w-full md:justify-center w-full rounded-3xl p-12 lg:flex md:flex  p-[42px] xl:p-20 lg:p-20 xl:flex-col lg:flex-col xl:flex">
 						<img src="./assets/img/aboutus.svg" class="max-w-full h-auto object-center " alt="..." />
 					</div>
 				</div>
--- src/components/NotifyMe/NotifyMe.js
+++ src/components/NotifyMe/NotifyMe.js
@@ -6,16 +6,18 @@ const axios = require('axios');
 const NotifyMe = () => {
   return (
     <div class="container mx-auto lg:px-20 xl:px-20 px-4" id="notifysection">
-      <div class="mt-20  xl:mt-40 lg:mt-40 bg-[#1e2029] rounded-3xl xl:p-28 	pb-20  	pt-20 ">
+		  <div class="mt-20  xl:mt-40 lg:mt-40 bg-[#1e2029] rounded-3xl xl:p-28 	pb-20  	pt-20 ">
         <h1 class="text-4xl text-white font-bold xl:text-center md:text-center lg:text-center text-left pl-5 lg:pl-0 xl:pl-0 font-si">Join the waitlist for early access</h1>
         <p class="text-white mt-6 font-[18px] font-sir text-left md:text-center xl:text-center lg:text-center pl-5 lg:pl-0 xl:pl-0" >Sign up for early access and latest news from ZKX.</p>
         <div class="xl:flex md:flex items-center justify-center ">
           <div class="xl:flex border-gray-200 rounded pt-12 lg:pl-12 xl:pl-12 pl-0">
             <form action="https://zkx.us20.list-manage.com/subscribe/post?u=72f9d82da7b5eebdbce96aa7b&id=8a292131d1" method="post">
               <input type="email" name="EMAIL"  class="outline-0	px-5 py-3 xl:w-80 md:w-80 lg:w-80 w-10/12  lg:py-3 xl:py-3 md:py-3 rounded-md font-sir" placeholder="Enter Your Email" required/>
-              <button class="outline-0	bg-[#00ff62] hover:bg-white text-white font-bold py-3 md:px-0 xl:ml-2 lg:ml-2 ml-0 xl:w-32 lg:w-32 px-4 font-sin mt-2 w-10/12 text-[#000] rounded items-center text-center" type="submit">Notify Me</button>
+              <button class="outline-0	bg-[#00ff62] hover:bg-white text-white font-bold py-3 md:px-0 xl:ml-2 lg:ml-2 ml-0 xl:w-32 lg:w-32 px-4 font-sin mt-2 w-10/12 text-[#000] rounded items-center text-center" type="submit">
+                Notify Me
+              </button>
             </form>
-          </div>
+				  </div>
         </div>
       </div>
     </div>
